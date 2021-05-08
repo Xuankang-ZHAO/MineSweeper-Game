22ca737b0a2b220420f658a99fc615a63aec47c0
DZ2584793260 <2584793260@qq.com>
2021-05-08 12:00:30 +0800

    test

--- src/openWindow/InitialWindow.java
+++ src/openWindow/InitialWindow.java
@@ -195,6 +195,9 @@ public class InitialWindow extends JFrame implements ActionListener {
                 }
             }
         }
+
+
+
         bufferedReader.close();
         return readDemo;
     }
