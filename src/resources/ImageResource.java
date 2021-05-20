2cd6f6a0363fd88f824df385f47a339866fad3f7
Tiga <12010326@mail.sustech.edu.cn>
2021-05-20 16:08:06 +0800

    加了新图片，皮肤的感觉出来了

--- src/resources/ImageResource.java
+++ src/resources/ImageResource.java
@@ -13,21 +13,21 @@ public class ImageResource {
     public static ImageIcon p3Plus = change(p3, 0.06);
     public static ImageIcon p4 = new ImageIcon("resouces/pictures/贴图史蒂夫.png");
     public static ImageIcon p4Plus = change(p4, 0.08);
-    public static ImageIcon p5 = new ImageIcon("resouces/pictures/草方块.PNG");
+    public static ImageIcon p5 = new ImageIcon("resouces/pictures/冰面.PNG");
     public static ImageIcon p5Plus = change(p5, 0.2);
-    public static ImageIcon p6 = new ImageIcon("resouces/pictures/钻石.PNG");
+    public static ImageIcon p6 = new ImageIcon("resouces/pictures/苦力怕贴图.PNG");
     public static ImageIcon p6Plus = change(p6, 0.11);
     public static ImageIcon p7 = new ImageIcon("resouces/pictures/钻石.PNG");
     public static ImageIcon p7Plus = change(p7, 0.06);
-    public static ImageIcon p8 = new ImageIcon("resouces/pictures/钻石.PNG");
+    public static ImageIcon p8 = new ImageIcon("resouces/pictures/贴图史蒂夫.png");
     public static ImageIcon p8Plus = change(p8, 0.08);
-    public static ImageIcon p9 = new ImageIcon("resouces/pictures/钻石.PNG");
-    public static ImageIcon p9Plus = change(p9, 0.2);
-    public static ImageIcon p10 = new ImageIcon("resouces/pictures/钻石.PNG");
-    public static ImageIcon p10Plus = change(p10, 0.11);
+    public static ImageIcon p9 = new ImageIcon("resouces/pictures/沙面.PNG");
+    public static ImageIcon p9Plus = change(p9, 0.12);
+    public static ImageIcon p10 = new ImageIcon("resouces/pictures/苦力怕贴图.PNG");
+    public static ImageIcon p10Plus = change(p10, 0.12);
     public static ImageIcon p11 = new ImageIcon("resouces/pictures/钻石.PNG");
     public static ImageIcon p11Plus = change(p11, 0.06);
-    public static ImageIcon p12 = new ImageIcon("resouces/pictures/钻石.PNG");
+    public static ImageIcon p12 = new ImageIcon("resouces/pictures/贴图史蒂夫.png");
     public static ImageIcon p12Plus = change(p12, 0.08);
 
     /**
--- src/selectMode/Background2.java
+++ src/selectMode/Background2.java
@@ -11,7 +11,7 @@ public class Background2 extends JPanel {
 
     public Background2() {
         try {
-            image = ImageIO.read(new File("src/pictures/背景.PNG"));
+            image = ImageIO.read(new File("resouces/pictures/背景.PNG"));
         } catch (IOException e) {
             e.printStackTrace();
         }
