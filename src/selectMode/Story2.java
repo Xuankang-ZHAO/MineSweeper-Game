ee9e00ee1feb1ac947357caec7261293cb252521
Tiga <12010326@mail.sustech.edu.cn>
2021-05-24 19:18:58 +0800

    the end

--- src/selectMode/Story2.java
+++ src/selectMode/Story2.java
@@ -20,9 +20,17 @@ public class Story2 extends JFrame {
         this.getLayeredPane().add(label, new Integer(Integer.MIN_VALUE));
 
         JLabel story22=new JLabel();
-        story22.setText("从前有两个小狗狗");
-        story22.setBounds(30, 70, 570, 330);
-        story22.setForeground(Color.WHITE);
+        story22.setText("<html>从前有一只小鸡和一头小牛在开心玩耍时穿越到了我的世界，" +
+                "正当它们迷茫时天空中出现了以下游戏规则" +
+                "【可通过点击格子获得数字来推测周围格子的状态" +
+                "左击点中苦力怕，苦力怕将爆炸并且扣除分数；" +
+                "右击点中史蒂夫，史蒂夫注视你，增加一次失误数；" +
+                "右击点中钻石，恭喜你，增加一分。" +
+                "尽可能的怎加你的分数并且减少你的失误数" +
+                "只有胜者才能逃出游戏世界】" +
+                "于是它们开始了扫雷...<html>");
+        story22.setBounds(30, 0, 570, 360);
+        story22.setForeground(Color.BLACK);
         story22.setFont(new Font("楷体", Font.BOLD, 25));
         this.add(story22);
 
--- src/selectMode/Story3.java
+++ src/selectMode/Story3.java
@@ -20,9 +20,17 @@ public class Story3 extends JFrame {
         this.getLayeredPane().add(label, new Integer(Integer.MIN_VALUE));
 
         JLabel story33=new JLabel();
-        story33.setText("从前有两个小老鼠");
-        story33.setBounds(30, 70, 570, 330);
-        story33.setForeground(Color.WHITE);
+        story33.setText("<html>从前一只奋斗鸭和一直摸鱼鸭在开心玩耍时穿越到了我的世界，" +
+                "正当它们迷茫时天空中出现了以下游戏规则" +
+                "【可通过点击格子获得数字来推测周围格子的状态" +
+                "左击点中苦力怕，苦力怕将爆炸并且扣除分数；" +
+                "右击点中史蒂夫，史蒂夫注视你，增加一次失误数；" +
+                "右击点中钻石，恭喜你，增加一分。" +
+                "尽可能的怎加你的分数并且减少你的失误数" +
+                "只有胜者才能逃出游戏世界】" +
+                "于是它们开始了扫雷...<html>");
+        story33.setBounds(30, 0, 570, 360);
+        story33.setForeground(Color.BLACK);
         story33.setFont(new Font("楷体", Font.BOLD, 25));
         this.add(story33);
 
